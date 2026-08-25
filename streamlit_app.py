b9da5b9b7b768db1dd2b285beffbcddedf1e14ca
texaspandaa <summerfadedaway@gmail.com>
2026-08-25 15:14:15 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT
 # st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect('choose up to 5 ingredients:', 
                                   my_dataframe, 
@@ -45,6 +45,10 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
+
+        st.write(fruit_chosen)
+
+      
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('the search value for ', fruit_chosen, 'is ', search_on, '.')
 

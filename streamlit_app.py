6682c22e09e17487c22416fe1b1f6275f98fb3ab
texaspandaa <summerfadedaway@gmail.com>
2026-08-25 14:06:15 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,18 +44,18 @@ if ingredients_list:
 
     
 
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
-                    values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
-
-st.write(my_insert_stmt)
-# st.stop()
-
-time_to_insert = st.button('Submit Order')
-
-if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered,' + name_on_order +'!', icon="✅")
-
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
+                        values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+    
+    st.write(my_insert_stmt)
+    # st.stop()
+    
+    time_to_insert = st.button('Submit Order')
+    
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered,' + name_on_order +'!', icon="✅")
+  
 
 
 

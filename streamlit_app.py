b2267df5c22cd05c4da59880fe5831fe61efbf09
texaspandaa <summerfadedaway@gmail.com>
2026-08-25 15:18:38 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,9 @@ if ingredients_list:
       
         st.subheader(fruit_chosen + ' Nutrition Information ' )
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")  
+
+        st.write(smoothiefroot_response)
+      
         st_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width = True)
 
     
